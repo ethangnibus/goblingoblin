470f29a83ff21b48511cae8cbde440cc06c37bb5
ethangnibus <ethangnibus@berkeley.edu>
2023-10-13 03:13:43 +0200

    added goblin and light

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 4, 4, 4 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
 
 const loader = new GLTFLoader();
+
+const pointLight = new THREE.PointLight(0xffffff, 20); // Color: white, Intensity: 1
+pointLight.position.set(2, 2, 2); // Position of the light source
+scene.add(pointLight);
 
 
 
@@ -26,7 +26,6 @@ loader.load(
 	function ( gltf ) {
 
 		scene.add( gltf.scene );
-
 		gltf.animations; // Array<THREE.AnimationClip>
 		gltf.scene; // THREE.Group
 		gltf.scenes; // Array<THREE.Group>
@@ -51,9 +50,6 @@ loader.load(
 
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
